93f88b02dc6ae701e489444d5c6ae872029dd6c5
yeiniadames <yeiniadames15@gmail.com>
2024-11-22 20:12:55 -0400

    Add files via upload

---
+++ nacimiento.py
@@ -0,0 +1,11 @@
+NOMBRE = input(" introdusca su nombre ")
+
+AÑO = int(input( " introdusca el año que nacio "))
+
+ACTUAL = 2024
+
+EDAD = ACTUAL - AÑO
+
+print ( " hola " + NOMBRE + " su edad actual es: " + str(EDAD) )
+
+
